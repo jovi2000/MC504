9158746abe68c88ae70634a613571cd01f228ceb
João Barreira <jovi2000barreira@gmail.com>
2023-05-11 00:13:48 -0300

    11/04

--- projetomultithread/prod_cons.c
+++ projetomultithread/prod_cons.c
@@ -7,7 +7,7 @@
 #include <semaphore.h>
 #include "prod_cons.h" 
 
-#define NUM_THREADS 5
+#define NUM_THREADS 6
 #define NUM_MESAS 4
 #define NUM_CHEFFS 1 
 
@@ -33,10 +33,11 @@ void* chef(void* args) {
     
         // Produce
         char* x = "Food";
-        sleep(1); // random
+        sleep(1); // random TODO()
 
         // Adicona no buffer
         pthread_mutex_lock(&mutexBuffer);
+        printf("Cozinheiro coloca comida no buffer\n");
         buffer[count] = x;
         count++;
         pthread_mutex_unlock(&mutexBuffer);
@@ -49,39 +50,63 @@ void* chef(void* args) {
  * */
 void* mesa(void* args) {
     while (1) {
+        ArgsMesa *argsMesa = (ArgsMesa *) args;
         char* comida;
-        long idMesa = (long)args;
-        long idCliente;
+        long idMesa = argsMesa->id;
 
         sem_wait(&semFila);
         pthread_mutex_lock(&mutexFila);
-        //TODO(): pegar o primeiro cliente(com seu id) da fila e colocar na mesa
-        //idCLiente = fila.senha
+        p_no clienteAtual = argsMesa->start;
         pthread_mutex_unlock(&mutexFila);
 
         // Remove uma comida do buffer
         sem_post(&semPedidosFeitos);
         sem_wait(&semPedidosProntos);
+        printf("Cliente tenta entrar no buffer pra pegar comida\n");
         pthread_mutex_lock(&mutexBuffer);
+        printf("Cliente entra no buffer pra pegar comida\n");
         comida = buffer[count - 1];
         count--;
         pthread_mutex_unlock(&mutexBuffer);
 
         // Consome a comida
-        printf("%ld got %s\n", idMesa ,comida);
+        printf("Cliente %ld da mesa %ld comeu %s\n", clienteAtual->senha, idMesa ,comida);
         sleep(1); //random
 
         // Sai da mesa TODO()
     }
 }
 
-void* fila(void* args) {
+void* colocar_clientes_fila(void* args) {
+    long i = 0;
     while (1) {
-        long i = 0;
-        sleep(1);
-        //adiciona cliente i na fila
-        i++;
-        sem_post(&semFila);
+      Fila *fila = (Fila *) args;
+      sleep(5); // random TODO()
+
+      //Cria cliente
+      p_no novoCliente;
+      novoCliente = malloc(sizeof(Cliente));
+      novoCliente->senha = i;
+      novoCliente->next = NULL;
+
+      //Coloca cliente na fila
+      if (fila->start == NULL) {
+        printf("Chegou aqui1 %ld\n", i);
+        pthread_mutex_lock(&mutexFila);
+        fila->start = novoCliente;
+        fila->last = novoCliente;
+        pthread_mutex_unlock(&mutexFila);
+      }
+      else {
+        printf("Chegou aqui2 %ld\n", i);
+        pthread_mutex_lock(&mutexFila);
+        fila->last->next = novoCliente; // SEGFAULT AQUI!!
+        fila->last = fila->last->next;
+        pthread_mutex_unlock(&mutexFila);
+      }
+      sem_post(&semFila);
+      printf("Cliente %ld colocado na fila\n", i);
+      i++;
     }
 }
 
@@ -89,18 +114,37 @@ int main(int argc, char* argv[]) {
     srand(time(NULL));
     //quando for fazer a fila, precisa fazer algum sistema pra ver qual mesa(th[id]) tá disponivel pra passar como argumento de pthread_create
     pthread_t th[NUM_THREADS]; 
+    pthread_mutex_init(&mutexFila, NULL);
     pthread_mutex_init(&mutexBuffer, NULL);
     sem_init(&semPedidosFeitos, 0, NUM_MESAS);
     sem_init(&semPedidosProntos, 0, 0);
-    sem_init(&semPedidosProntos, 0, 100);
+    sem_init(&semFila, 0, 100);
     long i;
-    for (i = 0; i < NUM_THREADS; i++) {
+
+    // Cria fila:
+    Fila *fila;
+    fila = malloc(sizeof(Fila));
+    fila->start = NULL;
+    fila->last = NULL;
+
+    // Argumentos da mesa:
+    ArgsMesa vectorArgsMesa[NUM_MESAS];
+    for (long j = 0; j < 4; j++) {
+      vectorArgsMesa[j].start = fila->start;
+      vectorArgsMesa[j].id = j;
+    }
+
+    if (pthread_create(&th[0], NULL, colocar_clientes_fila, &fila) != 0) {
+      perror("Failed to create thread");
+    }
+
+    for (i = 1; i < NUM_THREADS; i++) {
         if (i < 2) {
             if (pthread_create(&th[i], NULL, chef, (void *)i) != 0) {
                 perror("Failed to create thread");
             }
         } else {
-            if (pthread_create(&th[i], NULL, mesa, (void *)i) != 0) {
+            if (pthread_create(&th[i], NULL, mesa, &vectorArgsMesa[i-2]) != 0) {
                 perror("Failed to create thread");
             }
         }
@@ -112,6 +156,8 @@ int main(int argc, char* argv[]) {
     }
     sem_destroy(&semPedidosFeitos);
     sem_destroy(&semPedidosProntos);
+    sem_destroy(&semFila);
     pthread_mutex_destroy(&mutexBuffer);
+    free(fila);
     return 0;
 }
--- projetomultithread/prod_cons.h
+++ projetomultithread/prod_cons.h
@@ -18,29 +18,34 @@ enum Food {
  * Estrutura da lista que será a fila
  * Cliente é uma senha enquanto está na fila, e ao entra no restaurante, ele vira uma Thread cujo ID é 'senha'.
  * */
-typedef struct Client {
+typedef struct Cliente {
 
     struct Cliente *next;  //aponta pro próximo da fila
 
-    int senha; // ID do cliente
+    long senha; // ID do cliente
     enum Food food; //tipo de comida que o Cliente pode pedir(ele já sabe com antecedência)
-} No;
+} Cliente;
 
 typedef struct Cliente *p_no;
+
+typedef struct Fila {
+    p_no start;
+    p_no last;
+} Fila;
+
+typedef struct ArgsMesa {
+    long id;
+    p_no start;
+} ArgsMesa;
 
 // p_no *start; // ponteiro pro início da fila
 // p_no *last; // ponteiro pro final da fila
 
 /***
- * Cria uma lista ligada
-*/
-p_no create_list();
-
-/***
  * Coloca mais um cliente na fila pra entrar no restaurante.
  * Atualiza o ponteiro de fim
 */
-void insert_node(p_no last, p_no client);
+void* colocar_clientes_fila(void* args);
 
 
 
